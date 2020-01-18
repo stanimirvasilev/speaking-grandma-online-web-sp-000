4b4d7697e962899ee9fb1c2d2a521638d8803ce7
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-01-18 19:00:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,14 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma()
   
-  If
+  If "I LOVE YOU GRANDMA!" == "I LOVE YOU GRANDMA!".upcase
+  puts ""
+  
+  
   elsif
 else
 end 
-
+end
 
 
 
